ea9964b3202e5c3640c8a7c024720b8b237e0115
Reagan Davenport <reagandavenport@Reagans-MacBook-Air-2.local>
2022-09-14 15:29:23 -0400

    changed name of logo to 'logo.png', added boiler code to navbar js and css

---
+++ src/Components/Navbar/nav.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './nav.css'
+
+import logo from "../../Assets/logo.png"
+
+const Nav = () => {
+    return(
+        <div className='Nav'>
+            <div className='Logo'>
+                <a href='/'><img src={logo}></img></a>
+            </div>
+            <div className='Links'>
+                <a href=''><p>Menu</p></a>
+                <a href=''><p>Location</p></a>
+            </div>
+            
+        </div>
+    )
+    };
+
+export default Nav;
