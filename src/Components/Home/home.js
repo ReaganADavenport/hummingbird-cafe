4deae5f70249a2f06340655f12656f72ed1839de
Reagan Davenport <reagandavenport@Reagans-MacBook-Air-2.local>
2022-09-14 16:40:28 -0400

    filling out nav and home, added a coffee beans png for the background, added nav and home to app.js

---
+++ src/Components/Home/home.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+import './home.css';
+
+
+const Home = () => {
+    return(
+        <div className="Home">
+          <div className='Text'>
+            <h1>Hummingbird Café</h1>
+          </div>
+          
+        </div>
+    )
+}
+
+export default Home;
