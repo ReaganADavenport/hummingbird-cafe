a0946500798f3f3dc605820bb30a3bce4c1ee157
Reagan Davenport <reagan.a.davenport@gmail.com>
2022-09-20 16:44:07 -0400

    added chaiTeaLatte and londonFog to the menu

--- src/Components/Menu/menu.js
+++ src/Components/Menu/menu.js
@@ -16,7 +16,9 @@ const Menu = () => {
             <Card itemName={"Latte"} itemIngredients={"expresso, steamed milk, milk foam"} itemContents={'latte'}></Card>
             <Card itemName={"Café Bombón"} itemIngredients={"expresso, condensed milk"} itemContents={'bombon'}></Card>
             <Card itemName={"Cappuccino"} itemIngredients={"expresso, steamed milk, milk foam"} itemContents={'cappuccino'}></Card>
-            <Card itemName={"Green Tea"} itemIngredients={"green tea"} itemContents={'greenTea'}></Card>
+            <Card itemName={"Green Tea"} itemIngredients={"macha green tea"} itemContents={'greenTea'}></Card>
+            <Card itemName={"Chai Tea Latte"} itemIngredients={"chai tea, steamed milk, milk foam"} itemContents={'chaiTeaLatte'}></Card>
+            <Card itemName={"London Fog"} itemIngredients={"earl grey tea, vanilla, lavender, brown sugar, steamed milk, milk foam"} itemContents={'londonFog'}></Card>
         </div>
 
       </div>
